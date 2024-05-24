f785a586f79ef47c6a538c4c1ef5c7ee1bf737ff
sayantanbhowmik <sayanbhowmik1995@gmail.com>
2024-05-24 22:24:59 +0530

    first commit

---
+++ src/javaPractice/FirstClass.java
@@ -0,0 +1,20 @@
+package javaPractice;
+
+public class FirstClass {
+
+	
+	public void getData()
+	{
+		System.out.println("Fuck off");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		FirstClass x=new FirstClass();
+		System.out.println(x.getData());
+		
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author User
+ *
+ */
+module java_Code_Practise {
+}
